013754f01e7da4733e707f9f000f3a340f64b797
JJasurdev <developernew0917@gmail.com>
2025-02-09 21:48:17 +0500

    E:/Developer/python/Pythonhomeworks/lesson-2

---
+++ check.py
@@ -0,0 +1,11 @@
+import math
+def check_even_or_odd():
+    number = int(input("Enter a number: "))
+
+
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+check_even_or_odd()
